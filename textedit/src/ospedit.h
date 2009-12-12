4586347c81dbdac6db9158d8991c0859e4454086
Owen Rudge <owen@owenrudge.net>
2009-12-12 21:24:41 +0000

    Tidy up indentation

--- textedit/src/ospedit.h
+++ textedit/src/ospedit.h
@@ -1,6 +1,6 @@
 /*************************************************************/
 /* OSPlus - Open Source version                              */
-/* Copyright (c) Owen Rudge 2000-2005. All Rights Reserved.  */
+/* Copyright (c) Owen Rudge 2000-2009. All Rights Reserved.  */
 /*************************************************************/
 /* OSPlus Text Editor - Standalone                           */
 /* OSPEDIT.EXE                                               */
@@ -10,6 +10,7 @@
  *
  * 05/01/2002: Cleaned up indentation
  * 13/10/2002: Added MSVC support
+ * 12/12/2009: Tidy up intendation again
  */
 
 #include "ospver.h"
@@ -27,67 +28,66 @@ extern char __os_ver[50];
 #define OSP_OS_VER     __os_ver
 
 #if defined(__MSDOS__) && defined(__BORLANDC__)
-      #define BUILD_VERSION     "Borland C++ version (DOS)"
-      #define OSP_COMPILER      "Borland C++"
-      #define OSP_COMPILER_VER  __bc_compiler_ver
-      #define SOUND_SUPPORT     1
+	#define BUILD_VERSION     "Borland C++ version (DOS)"
+	#define OSP_COMPILER      "Borland C++"
+	#define OSP_COMPILER_VER  __bc_compiler_ver
+	#define SOUND_SUPPORT     1
 
-      extern char __bc_compiler_ver[50];
+	extern char __bc_compiler_ver[50];
 #elif defined(__DJGPP__)
-      #ifdef DJGPP_NO_SOUND_SUPPORT
-         #define BUILD_VERSION   "DJGPP version (DOS), no sound"
-         #define SOUND_SUPPORT   0
-      #else
-         #define BUILD_VERSION   "DJGPP version (DOS), with sound"
-         #define SOUND_SUPPORT     1
-      #endif
-
-      #define OSP_COMPILER      "DJGPP"
-      #define OSP_COMPILER_VER  __dj_compiler_ver
-
-      extern char __dj_compiler_ver[50];
-#elif defined(__WIN32__)
-	#ifdef __BORLANDC__
-          #define BUILD_VERSION     "Borland C++ version (Win32)"
-          #define OSP_COMPILER      "Borland C++"
-          #define OSP_COMPILER_VER  __bc_compiler_ver
-          #define SOUND_SUPPORT     1
-
-          extern char __bc_compiler_ver[50];
-	#elif defined(__MINGW32__)
-          #define BUILD_VERSION	"MinGW32 version (Win32)"
-          #define OSP_COMPILER      "MinGW32"
-          #define OSP_COMPILER_VER  __gcc_ver
-          #define SOUND_SUPPORT     1
-
-          extern char __gcc_ver[50];
+	#ifdef DJGPP_NO_SOUND_SUPPORT
+		#define BUILD_VERSION   "DJGPP version (DOS), no sound"
+		#define SOUND_SUPPORT   0
 	#else
-          #define BUILD_VERSION     "MSVC version (Win32)"
-          #define OSP_COMPILER      "Microsoft Visual C++"
-          #define OSP_COMPILER_VER  __msvc_compiler_ver
-          #define SOUND_SUPPORT     1
-
-          #define __MSVC__          1
-          extern char __msvc_compiler_ver[50];
+		#define BUILD_VERSION   "DJGPP version (DOS), with sound"
+		#define SOUND_SUPPORT   1
 	#endif
 
-#elif defined(__LINUX__)
-      #ifdef LINUX_NO_SOUND_SUPPORT
-            #define BUILD_VERSION   "Linux version, no sound"
-            #define SOUND_SUPPORT   0
+	#define OSP_COMPILER          "DJGPP"
+	#define OSP_COMPILER_VER      __dj_compiler_ver
+
+	extern char __dj_compiler_ver[50];
+#elif defined(__WIN32__)
+	#ifdef __BORLANDC__
+		#define BUILD_VERSION     "Borland C++ version (Win32)"
+		#define OSP_COMPILER      "Borland C++"
+		#define OSP_COMPILER_VER  __bc_compiler_ver
+		#define SOUND_SUPPORT     1
+
+		extern char __bc_compiler_ver[50];
+	#elif defined(__MINGW32__)
+		#define BUILD_VERSION	  "MinGW32 version (Win32)"
+		#define OSP_COMPILER	  "MinGW32"
+		#define OSP_COMPILER_VER  __gcc_ver
+		#define SOUND_SUPPORT     1
+
+		extern char __gcc_ver[50];
 	#else
-            #define BUILD_VERSION   "Linux version, with sound"
-            #define SOUND_SUPPORT   1
-      #endif
+		#define BUILD_VERSION     "MSVC version (Win32)"
+		#define OSP_COMPILER	  "Microsoft Visual C++"
+		#define OSP_COMPILER_VER  __msvc_compiler_ver
+		#define SOUND_SUPPORT     1
 
-      #define OSP_COMPILER      "GNU GCC"
-      #define OSP_COMPILER_VER  __gcc_ver
+		#define __MSVC__  1
+		extern char __msvc_compiler_ver[50];
+	#endif
+#elif defined(__LINUX__)
+	#ifdef LINUX_NO_SOUND_SUPPORT
+		#define BUILD_VERSION   "Linux version, no sound"
+		#define SOUND_SUPPORT   0
+	#else
+		#define BUILD_VERSION   "Linux version, with sound"
+		#define SOUND_SUPPORT   1
+	#endif
 
-      extern char __gcc_ver[50];
+	#define OSP_COMPILER      "GNU GCC"
+	#define OSP_COMPILER_VER  __gcc_ver
+
+	extern char __gcc_ver[50];
 #else
-      #define BUILD_VERSION     "Build unknown"
-      #define OSP_COMPILER      "Unknown"
-      #define OSP_COMPILER_VER  "Unknown"
+	#define BUILD_VERSION     "Build unknown"
+	#define OSP_COMPILER      "Unknown"
+	#define OSP_COMPILER_VER  "Unknown"
 #endif
 
 #ifndef VERSION_INFORMATION_ONLY
@@ -103,19 +103,19 @@ const int
   cmChangeDrct     = 102,
   cmShowClip       = 105,
 #endif
-  cmCalcCmd        = 104,
+  cmCalcCmd	       = 104,
   cmAbout          = 106,
   cmAboutApp       = 107,
   cmSelectWAV      = 108,
-  cmPlayWAV        = 109,
-  cmStopWAV        = 110,
+  cmPlayWAV	       = 109,
+  cmStopWAV	       = 110,
   cmSelectMID      = 111,
-  cmPlayMID        = 112,
-  cmStopMID        = 113,
+  cmPlayMID	       = 112,
+  cmStopMID	       = 113,
   cmSaveDesktop    = 114,
   cmRestoreDesktop = 115,
-  cmVerInfo        = 116,
-  cmCnvInfo        = 117;
+  cmVerInfo	       = 116,
+  cmCnvInfo	       = 117;
 
 class TEditorApp : public TApplication
 {
